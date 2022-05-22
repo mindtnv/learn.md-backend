d66ee8fcd88f409cd312d4a56a9477d9d017b473
mindtnv <mindtnv@gmail.com>
2022-05-22 14:39:44 +0300

    Add learnPostHandler

---
+++ src/learn/post.ts
@@ -0,0 +1,41 @@
+﻿import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { CardService } from "../CardService";
+import { getBody } from "../utils";
+
+export interface LearnPostModel {
+  difficult: "easy" | "normal" | "hard";
+}
+
+export interface LearnPostParams {
+  id: string;
+}
+
+export const createLearnPostHandler = (
+  app: FastifyInstance,
+  cardService: CardService
+) => {
+  return async (
+    request: FastifyRequest<{ Body: string; Params: LearnPostParams }>,
+    reply: FastifyReply
+  ) => {
+    try {
+      if (!request.body) return reply.code(400).send();
+
+      const body = getBody<LearnPostModel>(request.body);
+      const difficult = body.difficult.toLowerCase();
+      if (!["easy", "normal", "hard"].some((x) => x === difficult))
+        return reply.code(400).send();
+
+      const card = await cardService.getCard(
+        Number.parseInt(request.params.id)
+      );
+      return await cardService.learnCardAsync({
+        id: card.id,
+        difficult,
+      });
+    } catch (e) {
+      console.error(e);
+      return reply.code(500).send();
+    }
+  };
+};
