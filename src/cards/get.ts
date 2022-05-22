66da6c674c0ad2980c5203b9251951266276d406
mindtnv <mindtnv@gmail.com>
2022-05-22 14:40:05 +0300

    Add cardPostHandler

---
+++ src/cards/get.ts
@@ -0,0 +1,16 @@
+﻿import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { CardService } from "../CardService";
+
+export const createCardsGetHandler = (
+  app: FastifyInstance,
+  cardService: CardService
+) => {
+  return async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      return await cardService.getActiveCardsAsync();
+    } catch (e) {
+      console.error(e);
+      return reply.code(500).send();
+    }
+  };
+};
